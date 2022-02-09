725acce489311d78e769a918e72ef02034a7ad4c
Anthony <anthonygavriel@gmail.com>
2022-02-09 13:46:57 +0000

    Testing instances

--- LogicScripts/MainFunc.cs
+++ LogicScripts/MainFunc.cs
@@ -1,4 +1,5 @@
 using System;
+using System.Collections.Generic;
 
 /// <summary>
 /// Displaying basic functionality and how certain objects work
@@ -7,16 +8,17 @@ using System;
 public class MainFunc
 {
 
-    public static CardStack InstantiateStack(ref CardStack cs, int cell1, int cell2)
+    private static CardStack InstantiateStack(ref CardStack cs, int cell1, int cell2)
     {
-        var xr = new XlsReader();
+        var xr = new XlsCardReader();
 
         xr.WriteStack(cs, cell1, cell2);
         return cs;
     }
-    
-    public static void Main(string[] args)
+
+    public static Board InstantiateBoard()
     {
+        
         // No magic numbers necessary
         const int potLuckDetailsBegin = 5;
         const int potLuckDetailsEnd = 21;
@@ -28,15 +30,31 @@ public class MainFunc
 
         InstantiateStack(ref potLuck, potLuckDetailsBegin, potLuckDetailsEnd);
         InstantiateStack(ref oppKnocks, oppKnocksDetailsBegin, oppKnocksDetailsEnd);
+
+        var xr = new XlsSpaceReader();
+
+        List<Space.Property> properties = xr.WritePropertySpaces();
+        List<Space.Utility> utilities = xr.WriteUtilitySpaces();
+        List<Space.Station> stations = xr.WriteStationSpaces();
+        Space.Go go = xr.WriteGoSpace();
+        Space.IncomeTax incomeTax = xr.WriteIncomeTaxSpace();
+        Space.FreeParking freeParking = xr.WriteFreeParkingSpace();
+        Space.SuperTax superTax = xr.WriteSuperTaxSpace();
+        Space.GoToJail goToJail = xr.WriteGoToJailSpace();
+        Space.JustVisiting justVisiting = xr.WriteJustVisitingSpace();
+        List<Space.OpportunityKnocks> opportunityKnocksSpace = xr.WriteOpportunityKnocksSpace();
+        List<Space.PotLuck> potLucksSpace = xr.WritePotLuckSpaces();
+
+        return new Board(properties, utilities, stations, go, incomeTax, freeParking, superTax,
+            goToJail, justVisiting, opportunityKnocksSpace, potLucksSpace, oppKnocks, potLuck);
+    }
+
+public static void Main(string[] args)
+    {
+        // Instantiates 2 card decks, all spaces of the board and contains them all in one class
+        var board = InstantiateBoard();
         
-        
-        
-        potLuck.ShuffleStack();
-        potLuck.PrintCards();
-        
-        Console.WriteLine("\n\n");
-        
-        oppKnocks.ShuffleStack();
-        oppKnocks.PrintCards();
+        //Prints all spaces and cards
+        board.getDetails();
     }
 }
