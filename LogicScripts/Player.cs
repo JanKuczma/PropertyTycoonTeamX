72fbe1ffd889ac56b687f91e9d7165e4b32e7873
Anthony <anthonygavriel@gmail.com>
2022-02-09 16:25:52 +0000

    Updateq

--- LogicScripts/Player.cs
+++ LogicScripts/Player.cs
@@ -1,11 +1,38 @@
+using System;
+using System.Collections.Generic;
 using System.Security.Permissions;
+using OfficeOpenXml.Drawing.Chart;
 
 public class Player
 {
- private string name;
- // private Piece token;
- private Board loocation;
- 
- 
- 
+    private string name;
+    private Piece token; 
+
+     public Player()
+     {
+       Console.WriteLine("Enter a name");
+       this.name = Console.ReadLine();
+       Console.WriteLine("Welcome " + name +"!");
+       Console.WriteLine();
+     }
+
+     //Throws an error
+     public void PickPiece(ref List<Piece> pieces)
+     {
+         Console.WriteLine("Pick a piece: ");
+         Console.WriteLine();
+         for (int i = 0; i < pieces.Count; i++)
+         {
+             Console.WriteLine(i + " " + pieces[i]);
+         }
+
+         var choice = Console.Read();
+         token = pieces[choice];
+     }
+
+     public override string ToString()
+     {
+         return name + " using " + token;
+     }
 }
+
