ce66cf2cbe8c341735605927252e85e0fa693386
Anthony <anthonygavriel@gmail.com>
2022-02-10 19:20:26 +0000

    Update Bank
    Added some methods which get the game started such as starter money for players.

--- LogicScripts/Bank.cs
+++ LogicScripts/Bank.cs
@@ -1,5 +1,27 @@
+using System.Collections.Generic;
+
 public class Bank
 {
-    // To do
+    private int cash;
+    private Dictionary<Player, List<Space.Property>> playerItems;
+
+    public Bank()
+    {
+        cash = 50000;
+        playerItems = new Dictionary<Player, List<Space.Property>>();
+    }
+
+    public void starterMonies(ref List<Player> players)
+    {
+        foreach (var player in players)
+        {
+            player.ReceiveCash(1500);
+        }
+    }
+
+    private void printMoney()
+    {
+        cash = 50000;
+    }
 }
     
