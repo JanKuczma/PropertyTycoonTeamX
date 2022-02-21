289f12d6198b1f6f8b3d103319b5db01c8e19e2f
Anthony <anthonygavriel@gmail.com>
2022-02-21 16:40:56 +0000

    Prototypes

---
+++ Assets/Scripts/PrototypeClasses/Board.cs
@@ -0,0 +1,121 @@
+using System;
+using System.Collections.Generic;
+using OfficeOpenXml.Sparkline;
+
+public class Board
+{
+    //Board spaces
+    private List<Space.Property> properties;
+    private List<Space.Utility> utilities;
+    private List<Space.Station> stations;
+    private Space.Go go;
+    private Space.IncomeTax incomeTax;
+    private Space.FreeParking freeParking;
+    private Space.SuperTax superTax;
+    private Space.GoToJail goToJail;
+    private Space.JustVisiting justVisiting;
+    private List<Space.OpportunityKnocks> opportunityKnocksSpace;
+    private List<Space.PotLuck> potLucksSpace;
+    
+    //Bank
+    
+    
+    //Card decks
+    private CardStack oppKnocks;
+    private CardStack potLuck;
+
+    public Board(
+        List<Space.Property> properties, 
+        List<Space.Utility> utilities,
+        List<Space.Station> stations,
+        Space.Go go,
+        Space.IncomeTax incomeTax,
+        Space.FreeParking freeParking,
+        Space.SuperTax superTax,
+        Space.GoToJail goToJail,
+        Space.JustVisiting justVisiting,
+        List<Space.OpportunityKnocks> opportunityKnocksSpace,
+        List<Space.PotLuck> potLucksSpace,
+        CardStack oppKnocks,
+        CardStack potLuck
+    )
+    {
+        this.properties = properties;
+        this.utilities = utilities;
+        this.stations = stations;
+        this.go = go;
+        this.incomeTax = incomeTax;
+        this.freeParking = freeParking;
+        this.superTax = superTax;
+        this.goToJail = goToJail;
+        this.justVisiting = justVisiting;
+        this.opportunityKnocksSpace = opportunityKnocksSpace;
+        this.potLucksSpace = potLucksSpace;
+        this.oppKnocks = oppKnocks;
+        this.potLuck = potLuck;
+    }
+    
+    
+
+    public void ShufflePotLuck()
+    {
+        potLuck.ShuffleStack();
+    }
+
+    public void ShuffleOppKnocks()
+    {
+        oppKnocks.ShuffleStack();
+    }
+    
+    // Just for testing
+    public void GetDetails()
+    {
+        //Print board spaces
+        Console.WriteLine("Properties:");
+        foreach (var p in properties)
+        {
+            Console.WriteLine(p.ToString());
+        }
+        Console.WriteLine();
+        Console.WriteLine("Utilities:");
+        foreach (var u in utilities)
+        {
+            Console.WriteLine(u.ToString());
+        }
+        Console.WriteLine();
+        Console.WriteLine("Stations");
+        foreach (var s in stations)
+        {
+            Console.WriteLine(s.ToString());
+        }
+        Console.WriteLine();
+        Console.WriteLine("Action Spaces");
+        Console.WriteLine(go.ToString());
+        Console.WriteLine(incomeTax.ToString());
+        Console.WriteLine(freeParking.ToString());
+        Console.WriteLine(superTax.ToString());
+        Console.WriteLine(goToJail.ToString());
+        Console.WriteLine(justVisiting.ToString());
+        Console.WriteLine();
+        Console.WriteLine("Opp Knocks Space");
+        foreach (var o in opportunityKnocksSpace)
+        {
+            Console.WriteLine(o.ToString());
+        }
+        Console.WriteLine();
+        Console.WriteLine("Pot Luck Space");
+        foreach (var p in potLucksSpace)
+        {
+            Console.WriteLine(p.ToString());
+        }
+        Console.WriteLine();
+        
+        // Print cards
+        Console.WriteLine("Opp Knocks Cards");
+        oppKnocks.PrintCards();
+        Console.WriteLine();
+        Console.WriteLine("Pot Luck Cards");
+        potLuck.PrintCards();
+        Console.WriteLine();
+    }
+}
---
+++ Assets/Scripts/PrototypeClasses/Player.cs
@@ -0,0 +1,61 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+
+public class Player
+{
+    private string name;
+    private Piece token;
+    private int cash;
+    private Card getOutOfJail;
+
+    public Player()
+    {
+        Console.WriteLine();
+        Console.WriteLine("Enter a name");
+        this.name = Console.ReadLine();
+        Console.WriteLine("Welcome " + name +"!");
+        Console.WriteLine();
+        cash = 0;
+        getOutOfJail = null;
+    }
+
+    public void PickPiece(ref List<Piece> pieces)
+    {
+        Console.WriteLine("Pick a piece: ");
+        Console.WriteLine();
+        for (int i = 0; i < pieces.Count; i++)
+        {
+            Console.WriteLine(i+1 + ". " + pieces[i]);
+        }
+
+        var choice = Int32.Parse(Console.ReadLine());
+        choice -= 1;
+        token = pieces.ElementAt(choice);
+        pieces.RemoveAt(choice);
+        Console.WriteLine();
+    }
+
+    public void ReceiveCash(int cash)
+    {
+        this.cash += cash;
+    }
+     
+    /*
+     * Getters and ToString override
+     */
+    public string GetName()
+    {
+        return name;
+    }
+
+    public Piece GetToken()
+    {
+        return token;
+    }
+
+    public override string ToString()
+    {
+        return name + " is using the " + token + "\n" + name +" has £" + cash + "\n";
+    }
+}
