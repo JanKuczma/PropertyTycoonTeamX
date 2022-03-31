162d885f0da5a8d0f281b0843e7b6b7a2dad78ec
Jan Kuczma <jsk28@sussex.ac.uk>
2022-03-31 12:01:29 +0100

    and removed test code  ;-; x2

--- Assets/Scripts/controller/temp_contr.cs
+++ Assets/Scripts/controller/temp_contr.cs
@@ -64,9 +64,7 @@ public class temp_contr : MonoBehaviour
         foreach(Model.Player player in players)
         {
             pieces.Add(player,View.Piece.Create(player.token,transform,board_view));
-            player.cash = 0;
         }
-        players[0].cash = 10000;
         //setup finger cursor and get init cemara pos (top pos)
         Cursor.SetCursor(Asset.Cursor(CursorType.FINGER),Vector2.zero,CursorMode.Auto);
         cam_pos_top = Camera.main.transform.position;
